becfc0dfe8a654fdaf2eef8535b39e9c5fdb6749
Alvaro Bautista <alvarobp@gmail.com>
2012-02-16 22:03:51 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Vlctechhub::Application.config.secret_token = '2b3ab325dd90fb276433c49b9ccd42b32cf74286b5557b805c64627b88ea3bc0f1d507ad8a2751fbca7bf021946377bc4ae5af30e2d833f9c257574bad42fc5f'
