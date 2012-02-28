710b717eb79a502f09b3cd156b37d72ffdf48345
elmendalerenda <elmendalerenda@gmail.com>
2012-02-28 07:55:32 +0100

    added index.html

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,12 +1,12 @@
 class EventsController < ApplicationController
   respond_to :html, :json
-    
-  def index
-    @events = Event.all
-    
-    respond_with(@events)
-  end
-  
+
+  #def index
+  #  @events = Event.all
+
+  #  respond_with(@events)
+  #end
+
   def show
     @event = Event.find(params[:id])
   end
