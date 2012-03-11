5983ef749f50864a4fab429ac404d45764f6e63c
Alvaro Bautista <alvarobp@gmail.com>
2012-03-11 21:33:51 +0100

    Add validations to Event

--- app/models/event.rb
+++ app/models/event.rb
@@ -13,6 +13,13 @@ class Event
   field :icon
   field :follow
   
+  validates_presence_of :name
+  validates_presence_of :day
+  validates_presence_of :month
+  validates_presence_of :year
+  validates_format_of   :time, :with => Vlctechhub::TIME_FORMAT, :message => 'must be in format HH:MM'
+  validates_presence_of :organizer
+  
   def self.clone_month_events(year_from, month_from, year_to, month_to)
     events = Event.actual_month_events(year_from, month_from)
     events = Event.clean_events(events)
---
+++ lib/vlctechhub.rb
@@ -0,0 +1,3 @@
+module Vlctechhub
+  TIME_FORMAT = /(?:0?[0-9]|1[0-2]):(?:[0-5]\d|60)/
+end
